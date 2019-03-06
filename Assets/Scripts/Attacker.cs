6f8ff2c7944b152f618c9b7da69e39e088eca5e9
zeFerret <justinas.seskevicius@gmail.com>
2019-03-06 22:58:25 +0200

    feat: lizard attack on collision & health to defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
     float currentSpeed = 1f;
+    Animator animator;
+    GameObject currentTarget;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if(!currentTarget)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("IsAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(int damage)
+    {
+        if(!currentTarget) { return; }
+
+        Health targetHealth = currentTarget.GetComponent<Health>();
+
+        if(targetHealth)
+        {
+            targetHealth.DealDamage(damage);
+        }
     }
 }

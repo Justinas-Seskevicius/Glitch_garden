d8b45abb5ec7d7187d4a53a88e78673bb7939f9f
zeFerret <justinas.seskevicius@gmail.com>
2019-03-07 19:37:24 +0200

    feat: add fox attacker

---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Attacker attacker = otherCollider.gameObject.GetComponent<Attacker>();
+
+        if(attacker)
+        {
+            // TODO add some kind of animation event
+        }
+    }
+}
